6d62f4573622b247a1a535fd36c1ec86e95d3367
Jj <jjb9922@protonmail.com>
2025-02-15 23:56:49 +0000

    Parsing 6 bits

--- sim8086_zig/src/core/disassembler.zig
+++ sim8086_zig/src/core/disassembler.zig
@@ -1,8 +1,12 @@
 const std = @import("std");
 
 pub fn instructionFromBinaryOpcode(opcode: u8) ![]const u8 {
-    if (opcode == 0b10001001) {
+    // TODO: Separate out
+    const mask = 0b11111100;
+
+    if ((opcode & mask) == 0b10001000) {
         return "mov";
     }
+
     return "";
 }
