9a87df3d29a655ec9031f104cafef2f219851adf
Jj <jjb9922@protonmail.com>
2025-02-15 15:33:45 +0000

    basic file in

--- sim8086_zig/src/main.zig
+++ sim8086_zig/src/main.zig
@@ -1,3 +1,22 @@
 const std = @import("std");
 
-pub fn main() !void {}
+fn BinaryStringFromCompiledAsm() !void {
+    var file = try std.fs.cwd().openFile("./listings/listing37.asm", .{});
+    defer file.close();
+
+    var buffered = std.io.bufferedReader(file.reader());
+    var bufreader = buffered.reader();
+
+    var buffer: [1000]u8 = undefined;
+    @memset(buffer[0..], 0);
+
+    _ = try bufreader.readAll(buffer[0..]);
+
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print("{s}\n", .{buffer});
+}
+
+// Program Start
+pub fn main() !void {
+    _ = try BinaryStringFromCompiledAsm();
+}
